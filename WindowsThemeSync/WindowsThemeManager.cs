3356dbd7847871b74a9e425ed50cc2443b0dafb9
Amen Jlili <amen@bluebyte.biz>
2022-07-14 21:17:09 -0700

    Add project files.

---
+++ WindowsThemeSync/WindowsThemeManager.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Management;
+using System.Security.Principal;
+
+namespace WindowsThemeSync
+{
+
+    public enum Theme_e
+    {
+        Undefined,
+        Light,
+        Dark
+    }
+
+    public class WindowsThemeManager
+    {
+        #region Public Events
+
+        public static event EventHandler<Theme_e> WindowsThemeChanged;
+
+        #endregion
+
+        #region Private Fields
+
+        private static ManagementEventWatcher watcher;
+
+        #endregion
+
+        #region Private Properties
+
+        private static int OfficeVersion { get; set; }
+
+        #endregion
+
+        #region Public Properties
+
+        public static bool IsWatching { get; private set; } = false;
+
+        #endregion
+
+        #region Private Constructors
+
+        private WindowsThemeManager()
+        {
+        }
+
+        #endregion
+
+        #region Private Methods
+        private const string RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+        private const string RegistryValueName = "AppsUseLightTheme";
+
+        public static Theme_e GetWindowsTheme()
+        {
+            try
+            {
+                var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath);
+                var registryValueObject = key?.GetValue(RegistryValueName);
+                if (registryValueObject == null)
+                {
+                    return Theme_e.Light;
+                }
+
+                var registryValue = (int)registryValueObject;
+
+                key.Close();
+
+                return registryValue > 0 ? Theme_e.Light : Theme_e.Dark;
+            }
+            catch (Exception)
+            {
+
+                return Theme_e.Undefined;
+            }
+          
+
+
+        }
+        private static void Watcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            try
+            {
+                var ret = GetWindowsTheme();
+                WindowsThemeChanged?.Invoke(null, ret);
+            }
+            catch (System.Exception)
+            {
+                // needs to add logger here
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+         
+        public static void StartWatchingForThemeChanges()
+        {
+            try
+            {
+                if (IsWatching == true)
+                    throw new Exception("Object already watching registry entry");
+
+                var currentUser = WindowsIdentity.GetCurrent();
+                WqlEventQuery query = new WqlEventQuery(
+                "SELECT * FROM RegistryValueChangeEvent WHERE " +
+                "Hive = 'HKEY_USERS' " +
+                $@"AND KeyPath='{currentUser.User.Value}\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize' AND ValueName='{RegistryValueName}'");
+
+                watcher = new ManagementEventWatcher(query);
+                watcher.EventArrived -= Watcher_EventArrived;
+                watcher.EventArrived += Watcher_EventArrived;
+
+                watcher.Start();
+
+                IsWatching = true;
+            }
+            catch (Exception)
+            {
+
+                
+            }
+           
+        }
+
+        public static void StopWatchingForThemeChanges()
+        {
+            if (watcher != null)
+            {
+                watcher.Stop();
+                watcher.Dispose();
+                watcher = null;
+            }
+            IsWatching = false;
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsThemeSync/swAddIn.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using SolidWorks.Interop.swpublished;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsThemeSync
+{
+    [Guid("36EB446E-E8D2-4F2A-AE07-B45D8DD92694"), ComVisible(true)]
+    public class WindowsThemeSyncAddIn : ISwAddin
+    {
+        public SldWorks SOLIDWORKS { get; private set; }
+        public int SessionCookie { get; private set; }
+        public static object AddInName { get; private set; } = "Windows 10 App Mode Sync - Blue Byte Systems Inc.";
+        public static object AddInDescription { get; private set; } = "Syncs your SOLIDWORKS theme with Windows 10 default app mode.";
+
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            try
+            {
+                SOLIDWORKS = ThisSW as SldWorks;
+                SessionCookie = Cookie;
+                SOLIDWORKS.SetAddinCallbackInfo(0, this, SessionCookie);
+
+                RefreshTheme();
+                WindowsThemeManager.WindowsThemeChanged += WindowsThemeManager_OfficeThemeChanged;
+                WindowsThemeManager.StartWatchingForThemeChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Message = {e.Message} Stacktrace: {e.StackTrace}");
+
+                // Todo: log ex
+                return false;
+            }
+        }
+
+        private void WindowsThemeManager_OfficeThemeChanged(object sender, Theme_e e)
+        {
+
+            var currentTheme = (swInterfaceBrightnessTheme_e)SOLIDWORKS.GetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground);
+
+            switch (e)
+            {
+                case Theme_e.Light:
+                    if (currentTheme != swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Light)
+                    SOLIDWORKS.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground, (int)swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Light);
+                    break;
+                case Theme_e.Dark:
+
+                    if (currentTheme != swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Dark)
+                        SOLIDWORKS.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground, (int)swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Dark);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public void RefreshTheme()
+        {
+            var theme = WindowsThemeManager.GetWindowsTheme();
+            var currentTheme = (swInterfaceBrightnessTheme_e)SOLIDWORKS.GetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground);
+
+            switch (theme)
+            {
+                case Theme_e.Light:
+                    if (currentTheme != swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Light)
+                        SOLIDWORKS.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground, (int)swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Light);
+                    break;
+                case Theme_e.Dark:
+
+                    if (currentTheme != swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Dark)
+                        SOLIDWORKS.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swSystemColorsBackground, (int)swInterfaceBrightnessTheme_e.swInterfaceBrightnessTheme_Dark);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public bool DisconnectFromSW()
+        {
+            try
+            {
+                WindowsThemeManager.StopWatchingForThemeChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                //todo: log ex
+                return false;
+            }
+        }
+
+
+        [ComRegisterFunction]
+        private static void RegisterAssembly(Type t)
+        {
+            try
+            {
+                string KeyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
+                RegistryKey rk = Registry.LocalMachine.CreateSubKey(KeyPath);
+                rk.SetValue("Title", AddInName); // Title
+                rk.SetValue("Description", AddInDescription); // Description
+                rk.SetValue(null, 1); // startup parameter (loads addin automatically upon SOLIDWORKS startup)
+            }
+            catch (Exception e)
+            {
+                //todo: msgbox ex
+                throw e;
+            }
+        }
+
+        [ComUnregisterFunction]
+        private static void UnregisterAssembly(Type t)
+        {
+            try
+            {
+                bool Exist = false;
+                string KeyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
+                using (RegistryKey Key = Registry.LocalMachine.OpenSubKey(KeyPath))
+                {
+                    if (Key != null)
+                        Exist = true;
+                    else
+                        Exist = false;
+                }
+                if (Exist)
+                    Registry.LocalMachine.DeleteSubKeyTree(KeyPath);
+            }
+            catch (Exception e)
+            {
+                //todo: msgbox ex
+                throw e;
+            }
+        }
+    }
+}
